b832f9d41eb9ecf9d611e89ae9e208183f151c9a
Duda <106630667+mdudag@users.noreply.github.com>
2024-04-10 20:51:41 -0300

    Create code.py

---
+++ Relatorio1/metodo_bisseccao/ex3.1/code.py
@@ -0,0 +1,40 @@
+import math as math
+
+# Abrindo arquivo para leitura e obtenção dos dados
+with open('./bisseccao/file2.txt', 'r') as file:
+    f=eval(file.readline())
+    a=eval(file.readline())
+    b=eval(file.readline())
+    e=eval(file.readline())     # bi - ai
+    
+def bisseccao(f,a,b,e):
+    # Verificacao de raiz em [a,b]
+    if f(a)*f(b) >= 0:
+        print('Nao existe raiz')
+        exit()
+
+    # Analisando se já encontramos a raiz
+    x = (b+a)/2; f_x = f(x)
+
+    # Imprimindo valores
+    print(f'\na: {a}\nb: {b}\nx: {x}\nf(x): {f_x}\ne: {e}')
+
+    while abs(f_x) >= e:  
+        # Mudando intervalo    
+        if f(a)*f_x < 0:
+            b=x
+        else: a=x 
+
+        # Renovando valor de x
+        x = (b+a)/2; f_x = f(x)
+
+        # Imprimindo valores
+        print(f'\n\na: {a}\nb: {b}\nx: {x}\nf(x): {f_x}\ne: {e}')
+
+    return x
+
+print('\n============ Buscando Raiz ==============')
+val = bisseccao(f,a,b,e) 
+
+print('\n============ Resultados ==============')
+print(f'Raiz da Equacao de Kepler: {val}\nf(x) = {f(val)}\n')
