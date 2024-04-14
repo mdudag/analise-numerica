fe80b19ec3bc828d04121aed447b38ada9de3007
Duda <106630667+mdudag@users.noreply.github.com>
2024-04-13 21:27:35 -0300

    Update code.py

--- Relatorio1/Metodos_Iterativos_Sistemas/metodo_jacobi/ex5.2/code.py
+++ Relatorio1/Metodos_Iterativos_Sistemas/metodo_jacobi/ex5.2/code.py
@@ -25,14 +25,30 @@ b = np.array(vetor).reshape(n,1)
 
 # ==================== Condicao de Convergencia Diagonalmente Dominante ====================
 
-# Verificacao de colunas, depois linhas
+def sumLinhas(linha):
+    matriz_soma=0
+    for i in linha:
+        matriz_soma += i
+
+    return matriz_soma
+
+def max(x):
+    max=x[0]
+    for i in x[1:]:
+        if i>max:
+            max=i
+
+    return max
+
+# Verificacao de linhas, depois colunas
 i=0; ok=True
 while i<2:
     j=0; ok=True
     while j<n and ok:
-        matriz_soma = np.sum(A, axis=i)
+        if i==0: matriz_soma = sumLinhas(A[i])    # Linhas
+        else:    matriz_soma = sumLinhas(A[:,j])  # Colunas    
         
-        if A[j][j] <= matriz_soma[j]-A[j][j]:
+        if A[j][j] <= matriz_soma-A[j][j]:
             ok=False
         j+=1
     i+=1
@@ -42,34 +58,37 @@ while i<2:
 # ==================== Iteracoes de x ====================
 
 print('\n==================== Iteracoes ====================')
-if ok==True:    # Se o criterio foi atendido
-    x1=np.zeros((n,1))
-    print(f'\nx(0): \n{x1}')
+if ok==False:    # Se o criterio não foi atendido
+    print('\nCriterio nao atendido! \nNao há garantia de convergencia.')
 
-    c=1
-    while True:   # imitando estrutura "do while"
-        x0=np.array(x1)
+x1=np.zeros((n,1))
+print(f'\nx(0): \n{x1}')
 
-        i=0
-        while i<n:
-            xcomp=0
-            x1[i][0] = b[i][0]
+c=1
+while True:   # imitando estrutura "do while"
+    x0=np.array(x1)
 
-            j=0
-            while j<n:
-                if j!=i:
-                    x1[i][0] -= A[i][j]*x0[j][0]
-                j+=1
-            x1[i][0] /= A[i][i]
-            
-            i+=1
+    i=0
+    while i<n:
+        xcomp=0
+        x1[i][0] = b[i][0]
 
-        xcomp = np.max(np.abs(x1 - x0))/np.max(np.abs(x1))
-        print(f'\nx({c}): \n{x1}\n'
-              f'\nValor de convergencia: {xcomp}\n')
-        c+=1
+        j=0
+        while j<n:
+            if j!=i:
+                x1[i][0] -= A[i][j]*x0[j][0]
+            j+=1
+        x1[i][0] /= A[i][i]
+        
+        i+=1
 
-        if xcomp < e: break
+    xcomp = max(np.abs(x1 - x0))/max(np.abs(x1))
+    
+    print(f'\nx({c}): \n{x1}\n'
+            f'\nValor de convergencia: {xcomp}\n')
+    c+=1
+
+    if xcomp < e: break
 
 # ==================== Impressoes ==================== 
 
@@ -79,6 +98,6 @@ print(f'\nA:\n{A}\n'
       f'\ne: {e}\n'
       f'\nx de convergencia:\n{np.round(x1,4)}\n')
 
-print('\n==================== Aproximacao de A*x = b ====================\n')
+print('\n==================== Teste A*x = b ====================\n')
 print(f'Valor de b:\n{b}'
       f'\n\nValor de b resultante de A*x:\n{np.dot(A,x1)}\n')
